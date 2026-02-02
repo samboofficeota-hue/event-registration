ab8cb915c97a55335f4b1e7fabf3fb77dec4fde5
samboofficeota-hue <samboofficeota-hue@gmail.com>
2026-02-02 13:12:31 +0900

    管理画面にcss

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,7 +6,7 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen">
+    <div className="force-light flex min-h-screen">
       <AdminSidebar />
       <main className="flex-1 bg-gray-50 p-6">{children}</main>
     </div>
